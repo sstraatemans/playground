6d81782fee552b360c2f65bddb84de63ad98fc5a
Steven Straatemans <steven@straatemans.nl>
2025-11-05 12:25:17 +0100

    get the schema?

--- apps/graphserver/api/graphql.ts
+++ apps/graphserver/api/graphql.ts
@@ -1,10 +1,18 @@
+import { useDepthLimit } from '@envelop/depth-limit';
 import { createYoga } from 'graphql-yoga';
 import type { IncomingMessage, ServerResponse } from 'node:http';
-import { yoga as yogaServer } from '../src/server.js';
+import { schema } from '@/schema/index.js';
 
 // Create the Yoga instance
 const yoga = createYoga({
-  ...yogaServer,
+  schema,
+  graphiql: true, // UI at http://localhost:4000
+  plugins: [
+    useDepthLimit({
+      maxDepth: 3, // Set this to your desired limit (e.g., 5-10; test based on your schema complexity)
+      ignore: [], // Optional: Array of field names to ignore in depth calculation
+    }),
+  ],
   graphqlEndpoint: '/graphql',
 });
 
