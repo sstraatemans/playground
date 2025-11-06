4ddde5996005b7b5edb6c4c2f4467f0b2dab9807
Steven Straatemans <steven@straatemans.nl>
2025-11-06 23:38:41 +0100

    fix supabase link

--- apps/trpcserver/src/db/client.ts
+++ apps/trpcserver/src/db/client.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const prisma = new PrismaClient();
 
