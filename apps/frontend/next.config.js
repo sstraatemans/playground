650a98c31df2007ab98affb6699a3fc9cf9aed0e
Steven Straatemans <steven@straatemans.nl>
2025-11-06 16:03:39 +0100

    fix redirect

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@playground/trpcserver'],
+  async redirects() {
+    return [];
+  },
   async rewrites() {
     return [
       {
         source: '/graphql', // Matches /api/graph and any subpaths
-        destination: 'http://localhost:4001/graphql', // Proxies to external URL
+        destination: process.env.GRAPHQLSERVER, // Proxies to external URL
       },
     ];
   },
