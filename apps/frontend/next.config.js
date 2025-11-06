db1cf40ef61f9685854fc8407fd7d91db1170c5b
Steven Straatemans <steven@straatemans.nl>
2025-11-06 20:31:13 +0100

    try redirect

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,16 +2,17 @@
 const nextConfig = {
   transpilePackages: ['@playground/trpcserver'],
   async redirects() {
+    return [];
+  },
+  async rewrites() {
     return [
       {
         source: '/graphql', // Matches /api/graph and any subpaths
-        destination: `${process.env.GRAPHQLSERVER}`,
-        permanent: true,
+        destination:
+          process.env.GRAPHQLSERVER ||
+          'https://graphql.suskeenwiske.dev/graphql', // Proxies to external URL
       },
     ];
-  },
-  async rewrites() {
-    return [];
   },
 };
 
