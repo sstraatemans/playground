14be9cf73ec1ec291a6d6aca1c3417d4f408aaef
Steven Straatemans <steven@straatemans.nl>
2025-11-08 20:25:34 +0100

    fix the build

---
+++ apps/trpcserver/src/server.ts
@@ -0,0 +1,66 @@
+import cors from '@fastify/cors';
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
+import Fastify from 'fastify';
+import { createContext } from './server/trpc/context.js';
+import { appRouter } from './server/trpc/index.js';
+
+const app: Fastify.FastifyInstance = Fastify({
+  logger: true,
+});
+
+async function main() {
+  await app.register(cors, {
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps, curl, etc.)
+      // TODO: make something nice
+      if (!origin) return callback(null, true);
+
+      const allowedOrigins = ['http://localhost:3000'];
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
+
+  await app.register(fastifyTRPCPlugin, {
+    prefix: '/trpc/v1',
+    trpcOptions: {
+      router: appRouter,
+      createContext,
+      onError({ path, error }: any) {
+        app.log.error(`tRPC Error on ${path}:`, error);
+      },
+      // responseMeta(opts: any) {
+      //   const { paths, errors, type } = opts;
+      //   // Apply caching only for public queries with no errors
+      //   const allPublic = paths?.every((path: string) =>
+      //     path.includes('public')
+      //   );
+      //   const allOk = errors.length === 0;
+      //   const isQuery = type === 'query';
+
+      //   if (allPublic && allOk && isQuery) {
+      //     const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      //     return {
+      //       headers: {
+      //         'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+      //       },
+      //     };
+      //   }
+      //   return {};
+      // },
+    },
+  });
+
+  // 3️⃣ Start server
+  await app.listen({ port: 4000, host: '0.0.0.0' });
+  console.log('🚀 tRPC + Fastify ready on http://localhost:4000/trpc/v1');
+}
+
+main().catch((err) => {
+  console.error(err);
+});
