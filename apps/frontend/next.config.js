0e4a5a66f96846a8cba9c83a407bc73a12e7c39e
Steven Straatemans <steven@straatemans.nl>
2025-11-08 13:36:28 +0100

    lets try redirect

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,16 +2,23 @@
 const nextConfig = {
   transpilePackages: ["@sw/s_w_trpcserver"],
   async redirects() {
-    return [];
+    return [
+      {
+        source: "/api/graphql/v1",
+        destination:
+          "https://swgraphserver-git-fixbuild-steven-straatemans-projects.vercel.app/v1",
+        permanent: true,
+      },
+    ];
   },
   async rewrites() {
     return [
-      {
-        source: "/graphql/v1", // Matches /api/graph and any subpaths
-        destination:
-          process.env.GRAPHQLSERVER ||
-          "https://swgraphserver-git-fixbuild-steven-straatemans-projects.vercel.app/v1", // Proxies to external URL
-      },
+      // {
+      //   source: "/graphql/v1", // Matches /api/graph and any subpaths
+      //   destination:
+      //     process.env.GRAPHQLSERVER ||
+      //     "https://swgraphserver-git-fixbuild-steven-straatemans-projects.vercel.app/v1", // Proxies to external URL
+      // },
     ];
   },
 };
