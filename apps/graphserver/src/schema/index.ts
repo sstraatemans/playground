7a5029f4399de0d419cb938873e600022c8b56ae
Steven Straatemans <steven@straatemans.nl>
2025-11-08 13:27:32 +0100

    argh

--- apps/graphserver/src/schema/index.ts
+++ apps/graphserver/src/schema/index.ts
@@ -1,7 +1,7 @@
 import { builder } from './builder.js';
 import './queries.js';
 import './types/Album.js';
-import './types/AlbumSeriev';
+import './types/AlbumSerie.js';
 import './types/Artist.js';
 import './types/Character.js';
 import './types/Serie.js';
