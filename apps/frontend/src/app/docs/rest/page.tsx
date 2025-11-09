7c9bbf7cf70d8380f48bc9f558f6b4b32f60201f
Steven Straatemans <steven@straatemans.nl>
2025-11-09 19:49:05 +0100

    reverse the order of endpoints in swagger

--- apps/frontend/src/app/docs/rest/page.tsx
+++ apps/frontend/src/app/docs/rest/page.tsx
@@ -2,27 +2,26 @@ import Stack from "@/components/layout/Stack";
 import { Heading } from "@/components/Typography/Heading";
 import { Typography } from "@/components/Typography/Typography";
 import { createSwaggerSpec } from "next-swagger-doc";
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-
-const spec = createSwaggerSpec({
-  apiFolder: "src/app/api/v1",
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "",
-      version: "1.0.0",
-    },
-  },
-});
+import { Swagger } from "./Swagger";
 
 const Page = () => {
+  const spec = createSwaggerSpec({
+    apiFolder: "src/app/api/v1",
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "",
+        version: "1.0.0",
+      },
+    },
+  });
+
   return (
     <Stack direction="col">
       <Heading>REST API</Heading>
 
       <Typography>This is the rest api</Typography>
-      <SwaggerUI spec={spec} />
+      <Swagger spec={spec} />
     </Stack>
   );
 };
