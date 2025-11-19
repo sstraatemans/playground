9236befc8a717c65e67c1846cca96693bb51113e
Steven Straatemans <steven@straatemans.nl>
2025-11-19 12:40:20 +0100

    add favicons

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -21,7 +21,25 @@ const RootLayout = ({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="TRPC demo" />
         <meta content="text/html; charset=UTF-8" name="Content-Type" />
-        <meta content="#020E1B" name="theme-color" />
+        <meta content="#f50000" name="theme-color" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/icons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/icons/favicon.svg"
+        />
+        <link rel="shortcut icon" href="/assets/icons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/icons/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Suske en Wiske API" />
       </head>
       <body className="font-body bg-primary flex w-dvw max-w-dvw flex-col items-center">
         <Stack
