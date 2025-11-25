f4bc3545a73289fa5316594f3b79c3ce014af609
Steven Straatemans <steven@straatemans.nl>
2025-11-25 10:20:50 +0100

    test markdown

--- apps/test/src/app/page.tsx
+++ apps/test/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { trpcClient } from "@/utils/trpcClient";
 import { useState } from "react";
+import Markdown from "react-markdown";
 
 const Page = () => {
   const [data, setData] = useState<any>("[]");
@@ -10,7 +11,7 @@ const Page = () => {
       <h4>test of cross origin</h4>
       <p>can we use different packages on a different domain?</p>
 
-      <div className="flex content-between">
+      <div>
         <h5>trpc</h5>
         <button
           onClick={async () => {
@@ -20,7 +21,46 @@ const Page = () => {
         >
           get all
         </button>
+        <button
+          onClick={async () => {
+            const res = await trpcClient.characters.getCharacterById.query(1);
+            setData(res);
+          }}
+        >
+          get wiske
+        </button>
+        <hr />
+        <h5>rest</h5>
+        <button
+          onClick={async () => {
+            const res = await fetch(
+              "https://rest.suskeenwiske.dev/v1/albums",
+            ).then((res) => res.json());
+            setData(res);
+          }}
+        >
+          get all
+        </button>
+
+        <hr />
+        <h5>graphql (apollo client)</h5>
+        <button
+          onClick={async () => {
+            const res = await fetch(
+              "https://rest.suskeenwiske.dev/v1/albums",
+            ).then((res) => res.json());
+            setData(res);
+          }}
+        >
+          get all
+        </button>
       </div>
+
+      <hr />
+      <hr />
+      <h5>results</h5>
+
+      {data.description && <Markdown>{data.description}</Markdown>}
 
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
